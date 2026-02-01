56e4c3cbc97b440b325c0dddc93c4d7a05854fee
Ed Hansen <43901812+Isgotkowitz@users.noreply.github.com>
2026-02-01 21:00:27 +0900

    format test article controller

--- tests/test_routes/test_article_controller.py
+++ tests/test_routes/test_article_controller.py
@@ -1,11 +1,11 @@
-import pytest
-
 from litestar import Litestar
-from litestar.testing import AsyncTestClient
 from litestar.status_codes import HTTP_404_NOT_FOUND
+from litestar.testing import AsyncTestClient
 
 
-async def test_get_article_feed_no_token(test_client: AsyncTestClient[Litestar]) -> None:
+async def test_get_article_feed_no_token(
+    test_client: AsyncTestClient[Litestar],
+) -> None:
     response = await test_client.get("/api/feed")
 
     assert response.status_code == HTTP_404_NOT_FOUND
