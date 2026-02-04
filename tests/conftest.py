64f99f3c48f50272b6d7ecb2b3ca08810a7a5e01
Ed Hansen <43901812+Isgotkowitz@users.noreply.github.com>
2026-02-04 16:11:37 +0900

    removed unused fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,22 +6,6 @@ from pytest import fixture
 
 from app.main import app
 
-# @fixture(scope="function")
-# async def mock_user() -> User:
-#     sessionmaker = async_sessionmaker(expire_on_commit=False)
-#     async with sessionmaker(bind=app.state.engine) as session:
-#         user = User(
-#             username="mock_user",
-#             email="mock_email@mock.com",
-#             password="mock_pw",
-#             bio="mock_bio",
-#             image=None
-#         )
-#         session.add(user)
-#         await session.commit()
-#         await session.refresh(user)
-#     return user
-
 
 @fixture(scope="function")
 async def test_client() -> AsyncIterator[AsyncTestClient[Litestar]]:
